f790e31541ee5b8a726770ecd1830ae39aa96c5d
Minwooe <alsdn1412@naver.com>
2023-06-13 17:18:23 +0900

    기초

--- src/main/java/com/kdt/miniproject/controller/JoinCotroller.java
+++ src/main/java/com/kdt/miniproject/controller/JoinCotroller.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JoinCotroller {
  
- @RequestMapping("/join/")
+ @RequestMapping("/join")
  public String init(){
-  return "join";
+  return "/join/join";
  }
 }
