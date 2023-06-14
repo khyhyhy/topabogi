88b7fe8b5e33b4ce3462610eed88ca14373e1d2d
Minwooe <alsdn1412@naver.com>
2023-06-14 14:06:55 +0900

    수정6

--- src/main/java/com/kdt/miniproject/controller/LoginCotroller.java
+++ src/main/java/com/kdt/miniproject/controller/LoginCotroller.java
@@ -13,15 +13,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginCotroller {
  
     @RequestMapping("/login")
- 
     public String init() {
+
+
+
         return "/login/login";
     }
  
