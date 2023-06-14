8e7d983a0af1073f4685e5481ba026be1f378686
hamsang13 <hsn9300@gmail.com>
2023-06-14 11:29:51 +0900

    상세페이지 infovo api연결

---
+++ src/main/java/com/kdt/miniproject/vo/InfoVO.java
@@ -0,0 +1,14 @@
+package com.kdt.miniproject.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InfoVO {
+ String mapX,mapY,title,addr1,contentid,modifiedtime,firstimage2,tel;
+}
