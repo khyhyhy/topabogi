31c54d28d59891f0835717f52ef4508be1edb5a3
hamsang13 <hsn9300@gmail.com>
2023-06-15 11:21:41 +0900

    주변 정보 상세페이지 기초작업

--- src/main/java/com/kdt/miniproject/controller/InfoController.java
+++ src/main/java/com/kdt/miniproject/controller/InfoController.java
@@ -7,7 +7,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
