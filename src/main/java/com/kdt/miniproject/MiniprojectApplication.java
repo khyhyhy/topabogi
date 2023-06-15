f0c23669145633c14256647d853b86fdd3d9c29e
hamsang13 <hsn9300@gmail.com>
2023-06-15 14:32:32 +0900

    한글처리

--- src/main/java/com/kdt/miniproject/MiniprojectApplication.java
+++ src/main/java/com/kdt/miniproject/MiniprojectApplication.java
@@ -2,6 +2,8 @@ package com.kdt.miniproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class MiniprojectApplication {
@@ -10,4 +12,12 @@ public class MiniprojectApplication {
 		SpringApplication.run(MiniprojectApplication.class, args);
 	}
 
+ @Bean
+public CharacterEncodingFilter characterEncodingFilter() {
+	CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setEncoding("UTF-8");
+    characterEncodingFilter.setForceEncoding(true);
+    return characterEncodingFilter;
+
+}
 }
